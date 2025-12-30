6108b74ea3b60bb8fbd4df5b341c1ea400996387
Boahen Humphrey Agyenim-Boateng <112774674+Hboahen42@users.noreply.github.com>
2025-12-29 23:36:51 -0600

    Implement user CRUD

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,56 @@
+import logger from '#config/logger.js';
+import {jwttoken} from '#utils/jwt.js';
+
+export const authenticateToken = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.status(401).json({
+        error: 'Authentication required',
+        message: 'No access token provided',
+      });
+    }
+
+    req.user = jwttoken.verify(token);
+
+    logger.info(`User authenticated: ${req.user.email} (${req.user.role})`);
+    next();
+
+  } catch (error) {
+    logger.error('Authentication error:', error);
+    return res.status(401).json({
+      error: 'Unauthorized',
+      message: 'Invalid or expired token'
+    });
+  }
+};
+
+export const requireRole = (allowedRoles) => {
+  return (req, res, next) => {
+    try {
+      if (!req.user) {
+        return res.status(401).json({
+          error: 'User not authenticated',
+          message: 'Authentication required'
+        });
+      }
+
+      if (!allowedRoles.includes(req.user.role)) {
+        logger.error(`Access denied for user ${req.user.email} with role ${req.user.role}. Required: ${allowedRoles.join(', ')}`);
+        return res.status(403).json({
+          error: 'Access denied',
+          message: 'Admin access required'
+        });
+      }
+
+      next();
+    } catch (e) {
+      logger.error('Role verification error:', e);
+      return res.status(500).json({
+        error: 'Internal Server Error',
+        message: 'Error during role verification',
+      })
+    }
+  }
+};
